aa4d1c39db7b0d19e3cf161ce7730384b576bdf4
cocktailpeanut <cocktailpeanuts@proton.me>
2023-12-06 04:49:13 -0800

    init

---
+++ pinokio.js
@@ -0,0 +1,37 @@
+module.exports = {
+  title: "MagicAnimate Mini",
+  icon: "icon.gif",
+  description: "[NVIDIA GPU Only] An optimized version of MagicAnimate https://github.com/sdbds/magic-animate-for-windows",
+  menu: async (kernel) => {
+    let installed = await kernel.exists(__dirname, "env")
+    if (installed) {
+      let session = await kernel.require(__dirname, "session.json")
+      let running = await kernel.running(__dirname, "start.json")
+      if (running) {
+        return [{
+          icon: "fa-solid fa-spin fa-circle-notch",
+          text: "Running"
+        }, {
+          icon: "fa-solid fa-rocket",
+          text: "Open UI",
+          href: (session && session.url ? session.url : "http://127.0.0.1:7860"),
+          target: "_blank"
+        }]
+      } else {
+        return [{
+          icon: "fa-solid fa-power-off",
+          text: "Launch",
+          href: "start.json",
+          params: { fullscreen: true, run: true, mode: "single" }
+        }]
+      }
+    } else {
+      return [{
+        icon: "fa-solid fa-plug",
+        text: "Install",
+        href: "install.json",
+        params: { run: true, fullscreen: true }
+      }]
+    }
+  }
+}
