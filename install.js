2a6ed8e31a9e9f085349c15a52baa134243a3626
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-23 13:45:31 -0700

    update

--- install.js
+++ install.js
@@ -1,20 +1,20 @@
 module.exports = async (kernel) => {
-  let cmd = "uv pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/cpu"
+  let cmd = "pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/cpu"
   if (kernel.platform === 'darwin') {
     if (kernel.arch === "arm64") {
-      cmd = "uv pip install torch torchaudio torchvision"
+      cmd = "pip install torch torchaudio torchvision"
     } else {
-      cmd = "uv pip install torch==2.1.2 torchaudio==2.1.2"
+      cmd = "pip install torch==2.1.2 torchaudio==2.1.2"
     }
   } else {
     if (kernel.gpu === 'nvidia') {
       if (kernel.gpu_model && / 50.+/.test(kernel.gpu_model)) {
-        cmd = "uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128"
+        cmd = "pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128"
       } else {
-        cmd = "uv pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 xformers --index-url https://download.pytorch.org/whl/cu124"
+        cmd = "pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 xformers --index-url https://download.pytorch.org/whl/cu124"
       }
     } else if (kernel.gpu === 'amd') {
-      cmd = "uv pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/rocm6.2"
+      cmd = "pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/rocm6.2"
     } 
   }
   return {
@@ -33,8 +33,8 @@ module.exports = async (kernel) => {
         "build": true,
         "message": [
           cmd,
-          "uv pip install nvidia-pyindex",
-          "uv pip install -r requirements.txt",
+          "pip install nvidia-pyindex",
+          "pip install -r requirements.txt",
         ]
       }
     }, {
